d75317cdc9d8273f2825c513f20b409d69f9a5ea
unknown <victoriasugden@yahoo.co.uk>
2024-01-06 23:31:26 +0000

    deleted new code and reverted to previous

--- script.js
+++ script.js
@@ -1,71 +1,4 @@
-function generatePassword() {
-  var length = prompt("Enter the length of the password (between 8 and 128 characters):");
-
-  // Validate the length
-  if (length < 8 || length > 128) {
-      alert("Please enter a valid password length between 8 and 128 characters.");
-      return;
-  }
-
-  var lowercase = confirm("Include lowercase characters?");
-  var uppercase = confirm("Include uppercase characters?");
-  var numeric = confirm("Include numeric characters?");
-  var specialChars = confirm("Include special characters?");
-
-  // Validate at least one character type is selected
-  if (!lowercase && !uppercase && !numeric && !specialChars) {
-      alert("Please select at least one character type.");
-      return;
-  }
-
-  // Generate the password
-  var password = generateRandomPassword(length, lowercase, uppercase, numeric, specialChars);
-
-  // Display the password
-  alert("Generated Password: " + password);
-}
-
-function generateRandomPassword(length, lowercase, uppercase, numeric, specialChars) {
-  var charset = "";
-  var password = "";
-
-  if (lowercase) {
-      charset += "abcdefghijklmnopqrstuvwxyz";
-  }
-
-  if (uppercase) {
-      charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  }
-
-  if (numeric) {
-      charset += "0123456789";
-  }
-
-  if (specialChars) {
-      charset += "$@%&*,";
-  }
-
-  for (var i = 0; i < length; i++) {
-      var randomIndex = Math.floor(Math.random() * charset.length);
-      password += charset.charAt(randomIndex);
-  }
-
-  return password;
-}
-
-document.getElementById('generate').addEventListener('click', function() {
-  generatePassword();
-});
-
-
-
-
-
-
-
-
-
-/* Array of special characters to be included in password
+// Array of special characters to be included in password
 var specialCharacters = [
   '@',
   '%',
@@ -199,52 +132,31 @@ function getPasswordOptions() {
 
 //Step  1  Function for getting a random element from an array
 function getRandom(arr) {
-  //Math.random Math.floor
-  
-
-  
-return arr[Math.floor(Math.random()*arr.length)];
+    
+  genRand = arr[Math.floor(Math.random()*arr.length)];
+  return genRand;
      
 }
 
 
-function randomNum(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+
 
 // Function to generate password with user input
 function generatePassword() {
-
- var userOptions = getPasswordOptions()
-
-
-
- //Step 2 Based on each confirm value (e.g if they want a group of characters or not) you need to generate a passwordlet password = '';
-  
-
-const length = 12;
-const randomPassword = generatePassword(length);
-console.log(randomPassword);
-
- 
-
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
-
-  passwordText.value = password;
 }
+ 
+var userOptions = getPasswordOptions()
 
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
-
-*/
-  
+ // Get references to the #generate element
+ var generateBtn = document.querySelector('#generate');
+ 
+ // Write password to the #password input
+ function writePassword() {
+   var password = generatePassword();
+   var passwordText = document.querySelector('#password');
+ 
+   passwordText.value = password;
+ }
+ 
+ // Add event listener to generate button
+ generateBtn.addEventListener('click', writePassword);
